593be41e4e9d087f43ae5382a8dc16285e4110f4
sachin-83-maker <mca.sk11@gmail.com>
2025-08-04 09:46:09 +0530

    Add files via upload

---
+++ Week4/ArrayAssgn.js
@@ -0,0 +1,12 @@
+ const nums = [2,4,5,2,1,2]; 
+ const j=2
+ let k=0
+ for(let i=0;i<nums.length;i++)
+ {
+    if(nums[i]===j)
+    {
+        k++
+    } 
+
+ }
+   console.log(`count of variable is ${k}`)
